e9a84921e7740498688bf6789be664d363de1dcb
Mateusz Karcz <mateusz.karcz@interia.eu>
2022-07-07 22:13:53 +0200

    Create new project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Celones.MicrosoftKeyTool
+{
+    public static class Cruncher
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
